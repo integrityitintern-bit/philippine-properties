12fdb27243569b18a49c296a24a3ee5848171fcc
rylleairon-29 <rylleairon@gmail.com>
2026-05-05 15:01:28 +0800

    Initial commit: Astro real estate portal

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const WP_URL = 'https://philippine-properties.com';
+export const WP_API = 'https://philippine-properties.com/wp-json/wp/v2';
+export const SITE_NAME = 'Integrity Real Estate Corp';
+export const CONTACT_EMAIL = 'integrityitintern@integrityrealty.ph';
